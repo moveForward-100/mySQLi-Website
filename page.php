2872aea944e46ec22903bbfa0b596ac4cf7f5b75
e-shaan <eshaan.gupta101@gmail.com>
2022-01-11 18:03:12 +0530

    Cleaned code

--- page.php
+++ page.php
@@ -16,22 +16,23 @@
 		<div class="header-right">
 			<a href="signup.php">Sign Up</a>
 			<a class="active" href="index.php">Login</a>
-			<a href="#contact">Shop</a>
 			<a href="#about">About</a>
 		</div>
 	</div>
 
+	
 	<form class="choose"action="page.php" method="get">
 		<button class="types" type="submit" value="cats" name="cats">Cats</button>
 		<button class="types" type="submit" value="dogs" name="dogs">Dogs</button>
 	</form>
-
+	
+	<br><br>
 	<div class="wrapper">
 		<table class="table1">
 			<tr>
-				<th>Images</th>
-				<th>Names</th>
+				<th>Name</th>
 				<th>Price</th>
+				<th>Image</th>
 			</tr>
 			
 			<?php
@@ -46,17 +47,18 @@
 				die("Connection failed: " . $conn->connect_error);
 			}
 
-			$sql = "SELECT * FROM pet_details";
+			$sql = "SELECT pet_name , pet_type, price FROM pet_details";
 			$result = $conn->query($sql);
 
 			if ($result->num_rows > 0) 
 			{
 
+
 				while($row = $result->fetch_assoc()) 
 				{
 					
 					$img_name = "img\\" . $row['pet_name'] . ".jpg";
-					
+					//Displaying the pet_details contents
 					echo"<tr>".
 					"<td>". 
 					$row['pet_name'].
@@ -68,85 +70,93 @@
 					"<img src = '" . $img_name . "'>".
 					"</td>".
 					"</tr>";
-
-
-					//echo "Pet tyoe: " . $row["pet_type"]. " - Name: " . $row["pet_name"]. " " . $row["price"]. "<br>";
 				}
 			}
+			?>
+
+		</table>
+	</div>
+	<div class = "sqlquery">
+		<?php
+		echo "<div class='queryused'>SQL Query Used : <div class='query'>$sql;</div></div>";
+		?>
+	</div>
+
+	<footer>
+		<div class="credits">
+			<ul >
+				<li><h3><a target="_blank" href="http://moveforward100.com">www.moveforward100.com</a></h3></li>
+				<li>SQL Injection Practice Website</li>
+				<li>Happy Hacking!</li>
+			</ul>  
+		</div>
+	</footer>
+</body>
+</html>
+</html>
 
 
 
-
-			/*
-
+<?php
 
 
+/*
+$count = 0;
+			// output data of each row
+while($row = $result->fetch_assoc()) 
+{
+
+	<tr>
+	<td><img src='img/' $row['pet_name'] . "jpg"; "</td>
+	<td>"$row['pet_name']"</td>
+	<td>"$row['price']"</td>
+
+	</tr>
 
 
-			$count = 0;
-	  						// output data of each row
-			while($row = $result->fetch_assoc()) 
-			{
 
-				?>
-				<tr>
-					<td><img src='img/' $row['pet_name'] . "jpg"; "</td>
-					<td>"$row['pet_name']"</td>
-					<td>"$row['price']"</td>
+	$pet_type=$_GET['pet_type'];
+	if(isset($_GET['pet_type'])) {
+		$sql = "SELECT pet_type, pet_name, price FROM pet_details";
+		$result=mysqli_query($conn, $sql);
+		$row = mysqli_fetch_assoc($result);
+	} 
 
-				</tr>
-				<?php
-				$count = count + 1; ?>
-
-									<!--echo "<tr>". $img ."<td>". $row['pet_name'] ."</td><td>".$row['price'] ." $</td><td><button type='button' class = 'cart'><img src='img/".  $row['pet_name'] . ".jpg" . "></button></td></tr>";
-									-->
-
-								}
-							} 
+	elseif (isset($_GET['cats'])) {
+		header("Location: page.php?pet_type=cat");
+	}
+	else
+	{
+		header("Location: page.php?pet_type=dog");
+		exit;
+	}
 
 
-							$pet_type=$_GET['pet_type'];
-							if(isset($_GET['pet_type'])) {
-								$sql = "SELECT pet_type, pet_name, price FROM pet_details";
-								$result=mysqli_query($conn, $sql);
-								$row = mysqli_fetch_assoc($result);
-							} 
-
-							elseif (isset($_GET['cats'])) {
-								header("Location: page.php?pet_type=cat");
-							}
-							else
-							{
-								header("Location: page.php?pet_type=dog");
-								exit;
-							}
+	echo "<tr><th>Images</th><th>Names</th><th>Price</th><th>Add to cart</th></tr>";
 
 
-							echo "<tr><th>Images</th><th>Names</th><th>Price</th><th>Add to cart</th></tr>";
+	$row = mysqli_fetch_assoc($result);
 
+	while() 
+	{	
+		print_r($row);
 
-							$row = mysqli_fetch_assoc($result);
-
-							while() 
-							{	
-								print_r($row);
-
-								echo "$row['pet_name']";
+		echo "$row['pet_name']";
 								//$img = "<td><img src='img/$row['name'].JPG'></td>";
 
 
-								if($row['name']=='bull dog'){
-									$img = "<td><img src='img/bulldog.JPG'></td>";
-								}elseif($row['name']=='Bombay cat'){
-									$img = "<td><img src='img/bombay cat.jpg'></td>";
-								}elseif($row['name']=='german sheapord'){
-									$img = "<td><img src='img/german sheapord.jpg'></td>";
-								}elseif($row['name']=='pug'){
-									$img = "<td><img src='img/pug.jpg'></td>";
-								}elseif($row['name']=='Labrador'){
-									$img = "<td><img src='img/labrador.jpg'></td>";
-								}elseif($row['name']=='siamese'){
-									$img = "<td><img src='img/siamese.jpg'></td>";
+		if($row['name']=='bull dog'){
+			$img = "<td><img src='img/bulldog.JPG'></td>";
+			}elseif($row['name']=='Bombay cat'){
+				$img = "<td><img src='img/bombay cat.jpg'></td>";
+				}elseif($row['name']=='german sheapord'){
+					$img = "<td><img src='img/german sheapord.jpg'></td>";
+					}elseif($row['name']=='pug'){
+						$img = "<td><img src='img/pug.jpg'></td>";
+						}elseif($row['name']=='Labrador'){
+							$img = "<td><img src='img/labrador.jpg'></td>";
+							}elseif($row['name']=='siamese'){
+								$img = "<td><img src='img/siamese.jpg'></td>";
 								}elseif($row['name']=='british shorthair'){
 									$img = "<td><img src='img/brit.jpg'></td>";
 								}
@@ -158,26 +168,5 @@
 								}
 								echo "<tr>". $img ."<td>". $row['name'] ."</td><td>".$row['price'] ." $</td><td><button type='button' class = 'cart'><img src='img/addcart.png'></button></td></tr>";
 							}
-
-							*/
+*/
 							?>
-						</table>
-					</div>
-					<div class = "sqlquery">
-						<?php
-					//echo "<div class='queryused'>SQL Query Used : <div class='query'>$sql;</div></div>";
-						?>
-					</div>
-
-					<footer>
-						<div class="credits">
-							<ul >
-								<li><h3><a href="http://moveforward100.com">Moveforward</a></h3></li>
-								<li>SQL injection practice website</li>
-								<li>Ethical hacking L1</li>
-							</ul>  
-						</div>
-					</footer>
-				</body>
-				</html>
-				</html>
--- signup.php
+++ signup.php
@@ -1,113 +1,93 @@
 
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-  <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-    <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-      <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-        <head>
-          <meta charset='utf-8'>
 
-          <title>petshop</title>
-          <meta name='viewport' content='width=device-width, initial-scale=1'>
-          <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap" rel="stylesheet">
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+  <head>
+    <meta charset='utf-8'>
+
+    <title>petshop</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap" rel="stylesheet">
 
 
-        </head>
-        <body>
+  </head>
+  <body>
 
 
-          <div class="header"> 
-            <a href="index.php" class="name">Petshop.com</a>
-            <div class="header-right">
-              <a href="signup.php">Sign Up</a>
-              <a class="active" href="index.php">Login</a>
-              <a href="#contact">Shop</a>
-              <a href="#about">About</a>
+    <div class="header"> 
+      <a href="index.php" class="name">Petshop.com</a>
+      <div class="header-right">
+        <a href="signup.php">Sign Up</a>
+        <a class="active" href="index.php">Login</a>
+        <a href="#about">About</a>
+      </div>
+    </div>
+
+
+
+    <div class="main">
+      <div class="container-contact100">
+        <div class="wrap-contact100">
+          <form class="contact100-form validate-form"  action="signup.php" method="post">
+
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="text" name="user_name" placeholder="user_name" required>
+              <span class="focus-input100"></span>
             </div>
-          </div>
 
-
-
-          <div class="main">
-            <div class="container-contact100">
-              <div class="wrap-contact100">
-                <form class="contact100-form validate-form"  action="signup.php" method="post">
-
-                  <div class="wrap-input100 validate-input">
-                    <input class="input100" type="text" name="user_name" placeholder="user_name">
-                    <span class="focus-input100"></span>
-                  </div>
-
-                  <div class="wrap-input100 validate-input">
-                    <input class="input100" type="text" name="card_number" placeholder="card_number">
-                    <span class="focus-input100"></span>
-                  </div>
-
-                  <div class="wrap-input100 validate-input">
-                    <input class="input100" type="date" name="expiry_date" placeholder="expiry_date">
-                    <span class="focus-input100"></span>
-                  </div>
-
-                  <div class="wrap-input100 validate-input">
-                    <input class="input100" type="text" name="phone_number" placeholder="phone_number">
-                    <span class="focus-input100"></span>
-                  </div>
-
-                  <div class="wrap-input100 validate-input">
-                    <input class="input100" type="text" name="card_pin" placeholder="card_pin">
-                    <span class="focus-input100"></span>
-                  </div>
-
-
-                  <div class="wrap-input100 validate-input">
-                    <input class="input100" type="password" name="password" placeholder="password">
-                    <span class="focus-input100"></span>
-                  </div>
-
-
-                  <div class="container-contact100-form-btn">
-                    <button type = "submit" class="contact100-form-btn" type="submit" value="Signup" name="Signup"  >
-                      <span>
-                        <i class="fa fa-paper-plane-o m-r-6" aria-hidden="true"></i>
-                        Signup
-                      </span>
-                    </button>
-                  </div>
-                </form>
-              </div>
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="text" name="card_number" placeholder="card_number"required>
+              <span class="focus-input100"></span>
             </div>
-            <?php	
-        error_reporting(0);
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $db = "petshopdb";
-            $conn = mysqli_connect($servername, $username, $password , $db);
 
-            if (!$conn) {
-              die("Connection failed: " . mysqli_connect_error());
-            }
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="date" name="expiry_date" placeholder="expiry_date" required>
+              <span class="focus-input100"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="text" name="phone_number" placeholder="phone_number"required>
+              <span class="focus-input100"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="text" name="card_pin" placeholder="card_pin" required>
+              <span class="focus-input100"></span>
+            </div>
+
+
+            <div class="wrap-input100 validate-input">
+              <input class="input100" type="password" name="password" placeholder="password" required>
+              <span class="focus-input100"></span>
+            </div>
+
+
+            <div class="container-contact100-form-btn">
+              <button type = "submit" class="contact100-form-btn" type="submit" value="Signup" name="Signup"  >
+                <span>
+                  <i class="fa fa-paper-plane-o m-r-6" aria-hidden="true"></i>
+                  Signup
+                </span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <?php	
+      error_reporting(0);
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $db = "petshopdb";
+      $conn = mysqli_connect($servername, $username, $password , $db);
+
+      if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
 
     //mysqli_select_db('petshopdb', $conn);
 
-/*
-  user_details:
-
-  user_name  done
-  password done
-  phone_number
-
-  creditcard_details:
-
-  card_number
-  expiry_date
-  card_pin
-  user_name
-
-  
-*/
- 
   
   if($_POST['Signup']){
     $user_name=$_POST['user_name'];
@@ -145,11 +125,16 @@
   <footer>
     <div class="credits">
       <ul >
-        <li><h3><a href="http://moveforward100.com">moveForward</a></h3></li>
-        <li>SQL injection practice website</li>
-        <li>Ethical hacking L1</li>
+        <li><h3><a target="_blank" href="http://moveforward100.com">www.moveforward100.com</a></h3></li>
+        <li>SQL Injection Practice Website</li>
+        <li>Happy Hacking!</li>
       </ul>  
     </div>
   </footer>
 </body>
 </html>
+
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+  <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+    <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
